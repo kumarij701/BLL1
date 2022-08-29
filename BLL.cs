8a3c63312bcf4d119e75a3daf827b803d179b6f7
Kumari <2064588@cognizant.com>
2022-08-29 20:41:52 +0530

    first commit

---
+++ BLL.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.SqlClient;
+using DAL1;
+
+namespace BLL1
+{
+    public class BLL
+    {
+
+        public static SqlDataReader cmd;
+        public void Add1()
+        {
+           DAL1.DataAL dl1 =new DAL1.DataAL();
+
+            try {
+                SqlDataReader dr = dl1.RetrieveData();
+                Console.WriteLine("------------User Story Table---------------");
+                while (dr.Read())//loop rows
+                {
+                    for (int i = 0; i < dr.FieldCount; i++) //columns
+                    {
+                        Console.Write(dr[i] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+         }
+        
+    }
+}
+      /** static void Main()
+        {
+            DAL1.Class1 dal = new DAL1.Class1();
+            dal.RetrieveData();
+
+
+        }**/
